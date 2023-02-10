79b96afc72aaef221be5893b33eea1bce629dc26
Khang Inseok <nazehorn@gmail.com>
2023-02-10 15:03:06 +0900

    just git ~

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,15 +6,14 @@
 // use std::path::PathBuf;
 // use std::process::Command;
 use tauri::{Manager, PhysicalPosition, SystemTray, SystemTrayEvent};
-use window_vibrancy::apply_blur;
+use window_vibrancy::apply_acrylic;
 
 fn main() {
-    let tray = SystemTray::new();
+    // let tray = SystemTray::new();
     tauri::Builder::default()
         // .invoke_handler(tauri::generate_handler![
         //     execute,
         //   ])
-        // .plugin(window_vibrancy)
         .setup(|app| {
             let window = app.get_window("main").unwrap();
             let app_size = window.outer_size().unwrap();
@@ -26,18 +25,15 @@ fn main() {
                         mon.size().height - app_size.height - 30    // 30 -> taskbar height
                     )
                 ).unwrap();
-                // app_window.minimize().unwrap();
-                // app_window.show().unwrap();
             }
 
             #[cfg(target_os = "windows")]
-            apply_blur(&window, Some((18, 18, 18, 125)))
-                .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
-            // set_shadow(&window, true).expect("Unsupported platform!");
+            apply_acrylic(&window, Some((20, 20, 20, 128)))
+                .expect("Unsupported platform! 'apply_acrylic' is only supported on Windows");
             window.minimize().unwrap();
             Ok(())
         })
-        .system_tray(tray)
+        .system_tray(SystemTray::new())
         .on_system_tray_event(|app, event| if let SystemTrayEvent::LeftClick {
                 position: _,
                 size: _,
@@ -50,7 +46,6 @@ fn main() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
-
 
 // #[tauri::command]
 // fn execute(window: tauri::Window, file_path: &str) {
--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,52 @@
 import './style.scss'
-import { shell } from "@tauri-apps/api";
+import { shell} from "@tauri-apps/api";
 import { appWindow } from "@tauri-apps/api/window";
 import { register, isRegistered } from "@tauri-apps/api/globalShortcut";
+// import { BaseDirectory } from "@tauri-apps/api/fs";
 
-const $ = document.querySelector.bind(document);
-// const $$ = document.querySelectorAll.bind(document);
+const _ = document.querySelector.bind(document);
+// const __ = document.querySelectorAll.bind(document);
 
-$('#pageMain')!.addEventListener('click', async evt => {
+_('#pageMain')!.addEventListener('click', async evt => {
     const li = evt.target as HTMLElement;
     const filePath = li.dataset['filePath'];
-    if (filePath) {
-        shell.open(filePath);
-        appWindow.minimize();
+    // const dirPath = `C:\\Users\\nazhorn\\Desktop\\App's Shortcuts`;
+    if (filePath)  {
+        // const fp = `C:\\Users\\nazhorn\\Desktop\\App's Shortcuts\\${filePath}.lnk`;
+
+        // console.log(await fs.exists(`App's Shortcuts\\${filePath}.lnk`, { dir: BaseDirectory.Desktop }))
+        // if (!await fs.exists(fp))  {
+        //     console.log(fp);
+        //     return;
+        // }
+
+        await shell.open(`C:\\Users\\nazhorn\\Desktop\\App's Shortcuts\\${filePath}.lnk`);
+        await appWindow.minimize();
     }
 });
 
 // Window Control
-$('#pageHeader')!.addEventListener('click', async evt => {
+_('#pageHeader')!.addEventListener('click', async evt => {
     const ele = evt.target as HTMLElement;
     if (ele.tagName !== 'BUTTON') return;
 
     const cmd = ele.dataset['cmd'];
     switch (cmd) {
         case 'minimize':
-            appWindow.minimize();
+            await appWindow.minimize();
             break;
         case 'close':
-            appWindow.close();
+            await appWindow.close();
             break;
         default:
             console.log(`-- Invalid command passed ["${cmd}"]`);
     }
 });
+
+// window.kbdCheck = async (comb: string) => {
+//     const chk = await isRegistered(comb);
+//     console.log(`${comb} : ${chk}`);
+// }
 
 window.addEventListener('load', async _ => {
     const hotkey = 'Alt+Super+Enter';
@@ -39,19 +54,21 @@ window.addEventListener('load', async _ => {
 
     if (!isHotkeyRegistered) {
         await register(hotkey, async () => {
-            appWindow.unminimize();
-            appWindow.setFocus();
+            await appWindow.unminimize();
+            await appWindow.setFocus();
         });
     }
 });
 
-document.addEventListener('keydown', evt => {
+document.addEventListener('keydown', async evt => {
     if (evt.key === 'Escape')
-        appWindow.minimize();
+        await appWindow.minimize();
 });
 
-window.addEventListener('blur', _ => {
-    appWindow.minimize();
+// document.addEventListener('visibilitychange' , _ => console.log(document.visibilityState));
+
+window.addEventListener('blur', async _ => {
+    console.log(Date.now() + ' : blur')
+    // if ((await appWindow.outerPosition()).x > 0)
+    //     await appWindow.minimize();
 });
-
-
